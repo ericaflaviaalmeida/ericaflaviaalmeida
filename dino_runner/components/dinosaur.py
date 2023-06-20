525a7efd4dc9dc1875166bab837b0a66a7c4bbab
ericaflaviaalmeida <ericafalmeid@gmail.com>
2023-06-20 10:10:13 -0300

    aula 03: bird()

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,16 +1,20 @@
 import pygame
 from pygame.sprite import Sprite
 
-from dino_runner.utils.constants import RUNNING, JUMPING,DUCKING
+from dino_runner.utils.constants import RUNNING,DEFAULT_TYPE, JUMPING, DUCKING
 
 X_POS = 80
 Y_POS = 310
 Y_POS_DUCK = 340
 JUMP_VEL = 8.5
+RUN_IMG = {DEFAULT_TYPE: RUNNING}
+JUMP_IMG = {DEFAULT_TYPE: JUMPING}
+DUCK_IMG = {DEFAULT_TYPE: DUCKING}
 
 class Dinosaur(Sprite):
     def __init__(self):
-        self.image =  RUNNING[0]
+        self.type = DEFAULT_TYPE
+        self.image = RUN_IMG[self.type][0]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POS
@@ -45,14 +49,15 @@ class Dinosaur(Sprite):
             self.step_index = 0    
 
     def run(self):
-        self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
+        #self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
+        self.image = RUN_IMG[self.type][self.step_index // 5]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POS
         self.step_index += 1       
 
     def jump(self):
-        self.image = JUMPING
+        self.image = JUMP_IMG[self.type]
         if self.dino_jump:
             self.dino_rect.y -= self.jump_vel * 4 
             self.jump_vel -= 0.8
@@ -63,7 +68,7 @@ class Dinosaur(Sprite):
             self.jump_vel = JUMP_VEL
 
     def duck(self):
-        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.image = DUCK_IMG[self.type][self.step_index // 5]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POS_DUCK
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -2,25 +2,31 @@ import pygame
 import random
 
 from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants  import SMALL_CACTUS
+from dino_runner.components.obstacles.bird import Bird
+from dino_runner.utils.constants import *
 
 
 class ObstacleManager:
     def __init__(self):
         self.obstacles = []
         
-    def update(self, game):
-        if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))
 
+    def update(self, game):
+        self.image_obstacles = [Cactus(), Bird(),]
+        if len(self.obstacles) == 0:           
+                self.obstacles.append(self.image_obstacles[random.randint(0,1)]) # aleatorio aparecer a imagem        
+        
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
                 pygame.time.delay(500)
-                game.playing = False #encerra o loop do jogo
-                game.death_count += 1
-                break #encerra o loop do for obstacle in 
+                game.playing = False
+                break
+
+                                                                  
 
     def draw(self, screen):
         for obstacle in self.obstacles:
-            obstacle.draw(screen)        
+            obstacle.draw(screen)
+               
+      
